56131989b6a4921917527d4c23ee2cb4ce554676
Endaxe <94972517+Endaxe@users.noreply.github.com>
2023-04-21 11:08:23 +0200

    steg 1 klar.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+read();
+error();
 }
+
+    public static List read () {
+
+        List<String> List = new ArrayList<String>();
+
+        File ideaold = new File("ideaold.log");
+        Scanner Arraylist = null;
+        try {
+            Arraylist = new Scanner(ideaold);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (Arraylist.hasNext()) {
+            List.add(Arraylist.nextLine());
+        }
+        return List;
+
+
+
+        public static List error () {
+
+    }
+
+
+}
+
+
